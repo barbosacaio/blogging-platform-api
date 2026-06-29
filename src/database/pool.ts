439c4cd896d2fddfdae4d6d873b57051eb2a8050
barboscaio <caiiohenrique2004@gmail.com>
2026-06-29 13:49:44 -0300

    feat: database pool connection

---
+++ src/database/pool.ts
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+import 'dotenv/config';
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+});
+
+export default pool;
